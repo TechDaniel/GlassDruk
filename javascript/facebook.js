cab1803f6e80d21906537bc0eb37c4850a961069
TechDaniel <46842447+TechDaniel@users.noreply.github.com>
2021-07-12 11:36:13 +0200

    Update facebook.js

--- javascript/facebook.js
+++ javascript/facebook.js
@@ -1,14 +1,12 @@
 var chatbox = document.getElementById('fb-customer-chat');
 chatbox.setAttribute("page_id", "101346548892366");
 chatbox.setAttribute("attribution", "biz_inbox");
-
 window.fbAsyncInit = function() {
   FB.init({
-    xfbml: true,
-    version: 'v11.0'
+    xfbml            : true,
+    version          : 'v11.0'
   });
 };
-
 (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
